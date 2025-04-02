cf77edcd6222c3c544ba0f77b8f9b64cdb94aae2
Kevzon14 <kevinhoog41@gmail.com>
2025-04-02 20:54:13 +0200

    test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import tempfile
+
+
+message = "Nigger"
+
+
+temp_dir = tempfile.gettempdir()
+gandalf_message_file = os.path.join(temp_dir, "gandalf_message.txt")
+
+with open(gandalf_message_file, "w") as file:
+    file.write(message)
+
+subprocess.Popen(["notepad.exe", gandalf_message_file])
