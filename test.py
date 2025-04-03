0267d4da3b369b87465560732d68e46af576539e
Kevzon14 <kevinhoog41@gmail.com>
2025-04-03 13:59:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import subprocess
 import tempfile
 
 
-message = "Hacked By Pc Guy"
+message = "hanime.tv"
 
 
 temp_dir = tempfile.gettempdir()
@@ -12,4 +12,4 @@ gandalf_message_file = os.path.join(temp_dir, "gandalf_message.txt")
 with open(gandalf_message_file, "w") as file:
     file.write(message)
 
-subprocess.Popen(["notepad.exe", gandalf_message_file])
+subprocess.Popen(["Microsoft Edge.exe", gandalf_message_file])
